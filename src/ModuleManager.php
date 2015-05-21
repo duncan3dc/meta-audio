beda53cc3afcf2ce495c2fb12148539a935a63ea
Craig Duncan <git@duncanc.co.uk>
2015-05-22 00:36:44 +0100

    Create a trait for module management
    Add modules to a priority sequenced stack

---
+++ src/ModuleManager.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace duncan3dc\MetaAudio;
+
+use duncan3dc\MetaAudio\Modules\ModuleInterface;
+
+/**
+ * Manage which modules are active and their priority sequence.
+ */
+trait ModuleManager
+{
+    /**
+     * @var ModuleInterface[] $modules The modules used to read tags.
+     */
+    protected $modules = [];
+
+
+     /**
+     * Add a module to the stack.
+     *
+     * @param ModuleInterface The module object to add
+     *
+     * @return static
+     */
+    public function addModule(ModuleInterface $module)
+    {
+        $this->modules[] = $module;
+
+        return $this;
+    }
+
+
+    /**
+     * Add the default set of modules the library ships with.
+     *
+     * @return static
+     */
+    public function addDefaultModules()
+    {
+        $this->addModule(new Modules\Audio\Ape);
+        $this->addModule(new Modules\Audio\Mp3);
+
+        return $this;
+    }
+
+
+    /**
+     * Remove all previously defined modules.
+     *
+     * @return static
+     */
+    public function clearModules()
+    {
+        $this->modules = [];
+
+        return $this;
+    }
+}
