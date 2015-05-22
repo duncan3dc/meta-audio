907d2aa16b4e8606b78aaf7a1ae5064f14e56f25
Craig Duncan <git@duncanc.co.uk>
2015-05-22 19:58:24 +0100

    Create an abstract module to handle opening files

---
+++ src/Modules/AbstractModule.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace duncan3dc\MetaAudio\Modules;
+
+/**
+ * Base class for modules to extend
+ */
+abstract class AbstractModule implements ModuleInterface
+{
+    /**
+     * @var array|null $tags The parsed tags from the file.
+     */
+    protected $tags;
+
+    /**
+     * @var SplFileObject $file The file to read.
+     */
+    protected $file;
+
+
+    /**
+     * Load the passed file.
+     *
+     * @param SplFileObject $file The file to read
+     *
+     * @return static
+     */
+    public function open(\SplFileObject $file)
+    {
+        # If this file is already loaded then don't do anything
+        if ($this->file) {
+            $path1 = $this->file->getPath() . "/" . $this->file->getFilename();
+            $path2 = $file->getPath() . "/" . $file->getFilename();
+            if ($path1 === $path2) {
+                return $this;
+            }
+        }
+
+        $this->file = $file;
+        $this->tags = null;
+
+        return $this;
+    }
+
+
+    /**
+     * Get all the tags from the currently loaded file.
+     *
+     * @return array
+     */
+    abstract protected function getTags();
+
+
+    /**
+     * Get a tag from the file.
+     *
+     * @param string $key The name of the tag to get
+     *
+     * @return mixed
+     */
+    protected function getTag($key)
+    {
+        if (!is_array($this->tags)) {
+            $this->tags = $this->getTags();
+        }
+
+        if (!isset($this->tags[$key])) {
+            return "";
+        }
+
+        return $this->tags[$key];
+    }
+}
--- src/Mp3.php
+++ src/Mp3.php
@@ -40,6 +40,7 @@ class Mp3
     protected function getModuleString($method)
     {
         foreach ($this->modules as $module) {
+            $module->open($this->file);
             $result = $module->$method();
             if (is_string($result) && strlen($result) > 0) {
                 return $result;
@@ -62,6 +63,7 @@ class Mp3
     protected function getModuleInt($method)
     {
         foreach ($this->modules as $module) {
+            $module->open($this->file);
             $result = $module->$method();
             if (is_numeric($result) && $result > 0) {
                 return (int) $result;
