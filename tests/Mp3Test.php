71455082ce32a2bcba0dcd5811fbbd98d8f35d33
Craig Duncan <git@duncanc.co.uk>
2016-08-25 20:49:36 +0100

    Add some basic writing tests

--- tests/Mp3Test.php
+++ tests/Mp3Test.php
@@ -5,6 +5,7 @@ namespace duncan3dc\MetaAudioTests;
 use Mockery;
 use duncan3dc\MetaAudio\File;
 use duncan3dc\MetaAudio\Modules\ModuleInterface;
+use duncan3dc\MetaAudio\Modules\Ape;
 use duncan3dc\MetaAudio\Mp3;
 
 class Mp3Test extends \PHPUnit_Framework_TestCase
@@ -157,5 +158,35 @@ class Mp3Test extends \PHPUnit_Framework_TestCase
         $mp3->addModule($module2);
 
         $this->assertSame(4, $mp3->getTrackNumber());
+    }
+
+
+    public function testWrite()
+    {
+        $tmp = tempnam("/tmp", "meta-audio-");
+
+        $file = new File($tmp);
+
+        $mp3 = new Mp3($file);
+        $mp3->addModule(new Ape);
+
+        $artist = "Protest The Hero";
+        $year = 2010;
+
+        $mp3->setArtist($artist);
+        $mp3->setYear($year);
+
+        $this->assertSame($artist, $mp3->getArtist());
+        $this->assertSame($year, $mp3->getYear());
+
+        unset($mp3);
+
+        $mp3 = new Mp3($file);
+        $mp3->addModule(new Ape);
+
+        $this->assertSame($artist, $mp3->getArtist());
+        $this->assertSame($year, $mp3->getYear());
+
+        unlink($tmp);
     }
 }
